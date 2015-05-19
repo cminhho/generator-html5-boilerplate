e9b786062358f4bc6047bc4b1e7b6552f6a8c3a1
cminhho <hmchung2@gmail.com>
2015-05-19 12:55:13 -0700

    Update index.htlm

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
 module.exports = yeoman.generators.Base.extend({
   options: {
-    npm: false
+    npm: true
   },
   initializing: function() {
     this.pkg = require('../package.json');
@@ -13,25 +13,25 @@ module.exports = yeoman.generators.Base.extend({
     /**
      * Shows yeoman says his greatings unless the skip option is set
      */
-    this.log(yosay(chalk.red('Welcome!') + '\n' + chalk.yellow('You\'re using the fantastic generator for scaffolding an application with HTML5 and Gulp!')));
+	 this.log(yosay(
+      'You\'re using the fantastic generator for scaffolding an application with HTML5 and Gulp!'
+    ));
     var prompts = [{
-      type: 'confirm',
-      name: 'npm',
-      message: 'Would you like to skip install NPM dependencies?',
-      default: true
+      type: 'input',
+      name: 'name',
+      message: 'Your project name',
+      default : this.appname // Default to current folder name
     }];
     this.prompt(prompts, function(props) {
-      this.options.npm = props.npm;
+      this.log(props.name);
       done();
     }.bind(this));
   },
   writing: {
     app: function() {
       var paths = ['package.json', 'bower.json', 'gulpfile.js', '.gitignore'];
-      var libPaths = ['../bower_components/bootstrap/dist/css/bootstrap.min.css'];
       
 	  var dirs = ['app/icons', 'app/fonts', 'app/images', 'app/js/vendor', 'app/js/src', 'app/css/vendor', 'app/css/src', ];
-	  var libDirs = ['app/css/vendor'];
 	  
       this.fs.copy(this.templatePath('_index.html'), this.destinationPath('app/index.html'));
       for (var i = 0; i < dirs.length; i++) {
@@ -41,15 +41,12 @@ module.exports = yeoman.generators.Base.extend({
         this.fs.copy(this.templatePath('_' + paths[j]), this.destinationPath(paths[j]));
       }
 	  
-	  for (var y = 0; y < paths.length; y++) {
-        this.fs.copy(this.templatePath('_' + libPaths[y]), this.destinationPath(libDirs[y]));
-      }
     }
   },
   install: function() {
     this.installDependencies({
       bower: true,
-      npm: !this.options.npm
+      npm: true
     });
   }
 });
